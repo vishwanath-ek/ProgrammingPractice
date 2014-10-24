5a9fdf15556503659e9aad78eb92f696f954afd8
A Happy User <auser@nothing.none>
2014-10-23 23:48:32 -0700

    server mod

--- programs/server_client/server.c
+++ programs/server_client/server.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <errno.h>
+
+void
+error( const char *msg ){
+    fprintf(stderr, "%s -> %d: %s\n", msg, errno, strerr(errno));
+    exit(1);
+}
+
+int 
+main(){
+    int parent_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if( parent_socket == -1 ){
+        error("Error in socket creation");
+    }
+
+    return 0;
+}    
