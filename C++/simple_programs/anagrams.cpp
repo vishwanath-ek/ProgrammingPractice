f8f8372c35af62db50f761615e67771aee694797
Vizzard <vishwanath.ek@gmail.com>
2014-12-16 00:54:15 -0800

    Stores anagrams in map & prints it

--- C++/simple_programs/anagrams.cpp
+++ C++/simple_programs/anagrams.cpp
@@ -26,10 +26,32 @@ readFileAndMakeListOfAnagrams(std::map<std::string,
                 std::ifstream &in){
     in.open(FILE_NAME.c_str());
     std::string word;
-    while( !in.fail() && in >> word ){
+    while( !in.fail() && (in >> word) ){
+        // Sort the words in alphabetical order to get a unique key
+        // for anagrams
         const std::string signature = getSignature(word);
-        std::cout << signature <<std::endl;
+        final_map[signature].insert(word);
+        //std::cout << signature <<std::endl;
     }
+    return;
+}
+
+void
+printMap(std::map<std::string,
+         std::set<std::string> > &final_map){
+    std::map<std::string, std::set<std::string> >::const_iterator it_map;
+    std::set<std::string>::const_iterator it_set;
+    for(it_map = final_map.begin(); it_map != final_map.end(); ++it_map){
+        std::cout << it_map->first << ": ";
+        for(it_set = (it_map->second).begin(); it_set != (it_map->second).end(); ++it_set){
+            if( std::distance(it_set, (it_map->second).end()) > 1){                
+                std::cout << *it_set <<", ";
+            } else {
+                std::cout << *it_set << std::endl;
+            }
+        }
+    }
+    return;
 }
 
 int
@@ -39,6 +61,8 @@ main(){
     std::map<std::string, std::set<std::string> > final_map;
     std::ifstream in;
     readFileAndMakeListOfAnagrams(final_map, in);
+    printMap(final_map);
+    //getMaxNumberOfAnagrams(final_map);
 
     return 0;
 }
