9eed34a91e958cab99df5b36418ac70d95473d24
Vizzard <vishwanath.ek@gmail.com>
2015-03-31 18:01:35 -0700

    tcpp_pg480_11

---
+++ C++/simple_programs/tcpp_progs/tcpp_pg480_11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void func1(char, int, double, float){
+}
+
+int main(){
+    func1('a', 0, 1.0, 2.0);
+    return 1;
+}
---
+++ C++/simple_programs/tcpp_progs/tcpp_pg480_11.s
@@ -0,0 +1,113 @@
+	.file	"tcpp_pg480_11.cpp"
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.text
+	.globl	_Z5func1cidf
+	.type	_Z5func1cidf, @function
+_Z5func1cidf:
+.LFB971:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	8(%ebp), %eax
+	movb	%al, -4(%ebp)
+	movl	16(%ebp), %eax
+	movl	%eax, -16(%ebp)
+	movl	20(%ebp), %eax
+	movl	%eax, -12(%ebp)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE971:
+	.size	_Z5func1cidf, .-_Z5func1cidf
+	.globl	main
+	.type	main, @function
+main:
+.LFB972:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-8, %esp
+	subl	$24, %esp
+	movl	.LC0, %eax
+	movl	%eax, 16(%esp)
+	fld1
+	fstpl	8(%esp)
+	movl	$0, 4(%esp)
+	movl	$97, (%esp)
+	call	_Z5func1cidf
+	movl	$1, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE972:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB973:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	cmpl	$1, 8(%ebp)
+	jne	.L4
+	cmpl	$65535, 12(%ebp)
+	jne	.L4
+	movl	$_ZStL8__ioinit, (%esp)
+	call	_ZNSt8ios_base4InitC1Ev
+	movl	$__dso_handle, 8(%esp)
+	movl	$_ZStL8__ioinit, 4(%esp)
+	movl	$_ZNSt8ios_base4InitD1Ev, (%esp)
+	call	__cxa_atexit
+.L4:
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE973:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z5func1cidf, @function
+_GLOBAL__sub_I__Z5func1cidf:
+.LFB974:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$65535, 4(%esp)
+	movl	$1, (%esp)
+	call	_Z41__static_initialization_and_destruction_0ii
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE974:
+	.size	_GLOBAL__sub_I__Z5func1cidf, .-_GLOBAL__sub_I__Z5func1cidf
+	.section	.init_array,"aw"
+	.align 4
+	.long	_GLOBAL__sub_I__Z5func1cidf
+	.section	.rodata
+	.align 4
+.LC0:
+	.long	1073741824
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+	.section	.note.GNU-stack,"",@progbits
