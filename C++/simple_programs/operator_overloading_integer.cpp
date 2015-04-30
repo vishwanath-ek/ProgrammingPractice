fb8013ff6f042db64fb9ee52b076603d46c73b81
Vizzard <vishwanath.ek@gmail.com>
2015-04-30 11:07:00 -0700

    Operator overloading

--- C++/simple_programs/operator_overloading_integer.cpp
+++ C++/simple_programs/operator_overloading_integer.cpp
@@ -31,12 +31,32 @@ class Integer {
         friend Integer operator++(Integer&, int);
         
         // Similar to ++
-        friend Integer& operator--(Integer&);
-        friend Integer operator--(Integer&, int);
+        //friend Integer& operator--(Integer&);
+        //friend Integer operator--(Integer&, int);
 
         // += -=
-        friend Integer operator+=(Integer& integer, int);
-        friend Integer operator-=(Integer& integer, int);
+        friend Integer& operator+=(Integer& integer, int);
+        
+        // Member Functions ...
+        Integer& operator-=(int x){
+            std::cout << "Operator -= ... Memeber function" << std::endl;
+            i -= x;
+            return *this;
+        }
+
+        //Binary +
+        Integer operator+(const Integer& integer){
+            std::cout << "Operator +" << std::endl;
+            return Integer(i + integer.i);
+        }
+
+        // = has to be Memeber function as to overload the default
+        // Copy assignment bit-by-bit copy operator.
+        Integer& operator=(const Integer& integer){
+            std::cout << "Operator =" << std::endl;
+            i = integer.i;
+            return *this;
+        }
 };
 
 const Integer& operator+(const Integer& integer){
@@ -68,6 +88,12 @@ Integer operator++(Integer& integer, int){
     return intcopy;
 }
 
+Integer& operator+=(Integer& integer, int x){
+    std::cout << "Operator +=" << std::endl;
+    integer.i += x;
+    return integer;
+}
+
 int main(){
     Integer int1(23);
     +int1;
@@ -85,6 +111,32 @@ int main(){
     std::cout << "--- Postfix add ---"<< std::endl;
     (int1++).print();
     int1.print();
+
+    std::cout << "--- += Operator ---" << std::endl;
+    int1 += 2;
+    int1.print();
+
+    std::cout << "--- -= Operator ---" << std::endl;
+    int1 -= 2;
+    int1.print();
+
+    std::cout << "<---- Cascading ---->" << std::endl;
+    Integer int3(5);
+    // the + operator is overloaded as 2 is automatically
+    // converted to Integer class as constructor is not "explicit"
+    Integer int4 = ((int3++) + 2);
+    int4 -= 3;
+    int4.print();
+
+    std::cout << "----- = Operator -----" << std::endl;
+    Integer int5 = int4;
+    (int5 = int4) = int3;
+    std::cout << "int3: " << std::endl;
+    int3.print();
+    std::cout << "int4: " << std::endl;
+    int4.print();
+    std::cout << "int5: " << std::endl;
+    int5.print();
 
     return 1;
 }
